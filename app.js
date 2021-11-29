85f04d93f131bf68efa7ee5b7a22e1396a7f3a3d
peyDEV <93034361+peydevgit@users.noreply.github.com>
2021-11-29 23:47:47 +0100

    ytterligare modul express-session och felsökte ett fel
    Fick error när jag skulle starta servern angående när den läste in json filen. Löste det genom att lägga in "[ ]" i json filerna. samt la till variabeln guestbook som läser in filen guestbook.json.

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const users = (JSON.parse(fs.readFileSync('user.json'))); // Vi laddar in json filen user som vi kommer ha våra användarinformation på.
 module.exports = users;  // vi gör så den är tillgänglig för att skrivas.
+const guestbook = (JSON.parse(fs.readFileSync('guestbook.json'))); // Vi laddar in json filen user som vi kommer ha våra användarinformation på.
+module.exports = users;  // vi gör så den är tillgänglig för att skrivas.
 
 // start sidan
 app.get('/', function (req, res) {
